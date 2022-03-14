86d82c9301b517ce8b1d2e442e9c396679517f01
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2022-03-13 20:14:51 -0500

    This is my first commit

---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def main():
+    '''
+    This function takes a city and return 
+    the weather of that city.
+    '''
+    city = input("Give a city: ")
+    url = f'https://wttr.in/{city}'
+    weather = requests.get(url).text
+    print(weather)
+
+
+
+if __name__ == '__main__':
+    main()
